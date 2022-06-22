24740d852e4439ea3de61a7b49965fd6c4f0fe97
unknown <ishklianik@gmail.com>
2022-06-22 21:47:58 +0300

    update hw_js3

--- hw_js3.js
+++ hw_js3.js
@@ -11,14 +11,17 @@
 
 // п.1* преобразовать 1 в функцию принимающую на вход степень в которую будет возводится число
 
-// const checkpow=
-//  function(a) {
-// while(a<=10) {
-// result = Math.pow(2,a)  
-// console.log(a, result)
-// a ++}
-//  }
-// checkpow(8)
+const checkpow=
+
+function(a) {
+    let i=0
+    while (i<=a) {
+result = Math.pow(2,i)  
+console.log(i, result)
+i++}
+ }
+checkpow(10)
+
 
 // п.2 5 строк в консоль чтобы в первой выводился 1 смайл. во 2 - 2 и т.д. до 5
 
@@ -36,37 +39,40 @@
 // п.3 создать функцию которая принимает на вход  слово. Функция подсчитывает гласные и согласные и выдает в консоли
 // слово(ворд) состоит из (число) гласных и (число ) согласных
 
-// function getWordStructure(word) {
-//     let i = 0
-//     let result1 = 0
-//     let result2 = 0
-           
-//     while(i < word.length) {
+
+function getWordStructure(word) {
+        let i = 0
+        let result1 = 0
+        let result2 = 0
         
-//         let symb = word[i].toLowerCase()
-//         if(symb !== "-") {
-//             if(symb=="a" || symb=="e" || symb=="y" || symb=="u" || symb=="i" || symb=="o" ) 
-//             {
-//                 result1 +=1
-//             }
-//             else
-//             {
-//                 result2 +=1  
-//             }
-//         }    
-//         i++
-//     }
-    
-//     console.log("Слово" + " " + word + " " + "состоит из"+ " " + result1 + " " + "гласных и" + " " + result2 + " " + "согласных.");
-    
-// }
+           mm1=['a', "e", "y", "u", "i", "o"]   
+           mm2=["q", "w", "t", "r", "p", "s", "d", "f", "g", "h","j" ,"k", "l", "z", "x", "c", "v", "b","n", "m"]
+        
+           while(i < word.length) {
+                        let symb = word[i].toLowerCase()
+             
+                if(mm1.includes(symb)) 
+                {
+                    result1 +=1
+                }
+                 if(mm2.includes(symb)) 
+                {
+                    result2 +=1  
+                }
+                   
+            i++ 
+                     
+            }
+        console.log("Слово" + " " + word + " " + "состоит из"+ " " + result1 + " " + "гласных и" + " " + result2 + " " + "согласных.");
+                
+}    
+       getWordStructure('case')
+       getWordStructure('Case')
+       getWordStructure('Check-list')
 
-//    getWordStructure('case')
-//    getWordStructure('Case')
-//    getWordStructure('Check-list')
 
 
-//  п.4 Написать функцию , проверяющую является ли слово палиндромом
+       //  п.4 Написать функцию , проверяющую является ли слово палиндромом
 
 
 // function isPalindrom(word) {
@@ -96,31 +102,3 @@
 
 // isPalindrom("Abba")
 // isPalindrom("abba")
-
-
-function isPalindrom(word) {
-    let i = 0;
-    let len = word.length;
-    let a =  len/2;
-        
-    let res = "Word" + " " + word + " " + "is palindrom";
-                
-    while(i <= a-1) {
-            
-        let symb1 = word[i].toLowerCase();
-        let symb2 = word[len-1-i].toLowerCase();
-    
-        if(symb1!==symb2) 
-        {
-            res = "Word" + " " + word + " " + "is not palindrom";
-            break;
-        }
-            
-    i++}
-    
-    console.log(res) 
-    
-}
-
-isPalindrom("Abba")
-isPalindrom("abba")
